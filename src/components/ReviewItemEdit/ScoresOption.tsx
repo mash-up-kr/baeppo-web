fb71a7a5ac660b914eae5ce2d069b07cac9421e0
minsoopark <minsoopark@linecorp.com>
2021-04-30 01:51:31 +0900

    Add total score and star images

--- src/components/ReviewItemEdit/ScoresOption.tsx
+++ src/components/ReviewItemEdit/ScoresOption.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from "react";
+import Rating from "react-rating";
 import styled from "styled-components";
 
 interface ScoresOptionProps {
@@ -7,11 +8,38 @@ interface ScoresOptionProps {
 
 const ScoresOption: FC<ScoresOptionProps> = () => (
   <Wrapper>
-    <h3>총 점수</h3>
+    <Header>
+      <Title>총 점수</Title>
+      <Rating
+        fractions={2}
+        placeholderRating={3.5}
+        emptySymbol={<img src="/empty_star.svg" className="icon" />}
+        placeholderSymbol={<img src="full_star.svg" className="icon" />}
+        fullSymbol={<img src="full_star.svg" className="icon" />}
+      />
+      <TotalScore>3.5점</TotalScore>
+    </Header>
+
   </Wrapper>
 );
 export default ScoresOption;
 
 const Wrapper = styled.div`
   width: 100%;
+`;
+
+const Header = styled.header`
+  width: 100%;
+  display: flex;
+`;
+
+const Title = styled.span`
+  font-size: 18px;
+  margin: -1.7px 33px 0 0;
+`;
+
+const TotalScore = styled.span`
+  font-size: 18px;
+  color: #7E7E7E;
+  margin: -3.7px 0 0 9px;
 `;
